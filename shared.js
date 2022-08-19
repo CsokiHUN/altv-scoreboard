6cc11eedbb7affb1962cc1c2319aa11b8de19b5d
Csoki <feketetibi71@gmail.com>
2022-08-19 10:47:51 +0200

    add shared getTimestamp() function

---
+++ shared.js
@@ -0,0 +1,13 @@
+export function getTimestamp() {
+  const now = new Date();
+
+  return Date.UTC(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds(),
+    now.getUTCMilliseconds()
+  );
+}
